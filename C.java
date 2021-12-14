a7eeb36f6e5aec9dc3072eea95fbacecdc2b727f
scholasticLlama <96135653+scholasticLlama@users.noreply.github.com>
2021-12-14 18:47:22 +0200

    Add files via upload

---
+++ C.java
@@ -0,0 +1,13 @@
+package com.oop;
+
+public class C {
+    public static void main(String[] arg) {
+        Integer i3 = 100;
+        Integer i4 = 100;
+        if(i3 == i4) System.out.println("same obj");
+        if(i3.equals(i4)) System.out.println("equals");
+
+
+    }
+
+}
